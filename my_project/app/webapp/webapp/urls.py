c80ae0b2f05ec6106b6238ff37dd58d259080c00
swingc <swingccom@gmail.com>
2014-03-16 21:43:34 -0500

    Update urls.py

--- my_project/app/webapp/webapp/urls.py
+++ my_project/app/webapp/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from webapp.views import baojiabiao,baojia,instockbaby,jipiao, current_time,index,dr,hello, lljhb,llchb,sjyjh,sjyjhb,xjjchb,dealin,cgin,cginv,cgckb, My_Homepage_view#, search_papers
+from webapp.views import baojiabiao,baojia,instockbaby,jipiao, current_time,index,dr,hello, lljhb,llchb,sjyjh,sjyjhb,sjych,sjychb,dealin,cgin,cginv,cgckb, My_Homepage_view#, search_papers
 
 from django.contrib import admin
 admin.autodiscover()
@@ -20,7 +20,8 @@ urlpatterns = patterns('',
 	url('^llchb/$',llchb),
 	url('^sjyjhb/$',sjyjhb),
 	url('^sjyjh/$',sjyjh),
-	url('^xjjchb/$',xjjchb),
+	url('^sjychb/$',sjychb),
+	url('^sjych/$',sjych),
 		url('^xjjchb2/$',xjjchb),
 		url('^jipiao/$',jipiao),
 		url('^instockbaby/$',instockbaby),
