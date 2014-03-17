f5829821d67f0d0dad36f835c0fd9fd5c0ac467a
swingc <swingccom@gmail.com>
2014-03-16 21:06:22 -0500

    Update urls.py

--- my_project/app/webapp/webapp/urls.py
+++ my_project/app/webapp/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from webapp.views import baojia,instockbaby,jipiao, current_time,index,dr,hello, lljhb,llchb,xjjjhb,xjjchb,dealin,cgin,cginv,cgckb, My_Homepage_view#, search_papers
+from webapp.views import baojiabiao,baojia,instockbaby,jipiao, current_time,index,dr,hello, lljhb,llchb,xjjjhb,xjjchb,dealin,cgin,cginv,cgckb, My_Homepage_view#, search_papers
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,20 +9,21 @@ urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'webapp.views.index', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^time/',current_time),
+	url(r'^time/$',current_time),
     url(r'^admin/', include(admin.site.urls)),
-	url('^dealin/',dealin),
-	url('^dr/',dr),
-	url('^cgin/',cgin),
-	url('^cgckb/',cgckb),
-        url('^cginv/',cginv), 
-	url('^lljhb/',lljhb),
-	url('^llchb/',llchb),
-	url('^xjjjhb/',xjjjhb),
-	url('^xjjchb/',xjjchb),
-		url('^xjjchb2/',xjjchb),
-		url('^jipiao/',jipiao),
+	url('^dealin/$',dealin),
+	url('^dr/$',dr),
+	url('^cgin/$',cgin),
+	url('^cgckb/$',cgckb),
+        url('^cginv/$',cginv), 
+	url('^lljhb/$',lljhb),
+	url('^llchb/$',llchb),
+	url('^xjjjhb/$',xjjjhb),
+	url('^xjjchb/$',xjjchb),
+		url('^xjjchb2/$',xjjchb),
+		url('^jipiao/$',jipiao),
 		url('^instockbaby/$',instockbaby),
 			url('^baojia/$',baojia),
+			url('^baojiabiao/$',baojiabiao),
  
 )
