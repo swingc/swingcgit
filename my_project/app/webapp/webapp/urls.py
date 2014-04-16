51ec09f00aaf24488cec496797fa55140a76b3a0
swingc <swingccom@gmail.com>
2014-04-16 11:52:32 -0500

    Update urls.py

--- my_project/app/webapp/webapp/urls.py
+++ my_project/app/webapp/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from webapp.views import gcin,legolink,lishulink,baojiabiao,baojia,instockbaby,jipiao, current_time,index,dr,hello, lljhb,llchb,sjyjh,sjyjhb,sjych,sjychb,dealin,cgin,cginv,cgckb, My_Homepage_view#, search_papers
+from webapp.views import zhuoyajinhuo,gcin,legolink,lishulink,baojiabiao,baojia,instockbaby,jipiao, current_time,index,dr,hello, lljhb,llchb,sjyjh,sjyjhb,sjych,sjychb,dealin,cgin,cginv,cgckb, My_Homepage_view#, search_papers
 
 from django.contrib import admin
 admin.autodiscover()
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
 			url('^lishulink/$',lishulink),
 			url('^legolink/$',legolink),
 			url('^giftcardin/$',gcin),
+			url('^zyjh001/$',zhuoyajinhuo),
 			
  
 )
